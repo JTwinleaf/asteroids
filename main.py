377a910dea0a2ec669033d6d22841d750348c59f
JTwinleaf <jongarnett1996@gmail.com>
2024-12-02 14:16:06 -0600

    Changed to groups instead of methods for rendering things on screen

--- main.py
+++ main.py
@@ -11,16 +11,24 @@ def main():
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     dt = 0
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        for entity in updateable:
+            entity.update(dt)
+
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+
+        for entity in drawable:
+            entity.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
